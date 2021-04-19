43de229630c819e351b583ae45ea59b37b192eac
y.troniak <y.troniak@levi9.com>
2021-04-19 14:05:01 +0300

    ghp works

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBcmoe5cDrNfjurK9JoWxCzXL4X_Me7N8M',
+    authDomain: 'racoon-9e62c.firebaseapp.com',
+    projectId: 'racoon-9e62c',
+    storageBucket: 'racoon-9e62c.appspot.com',
+    messagingSenderId: '98785938897',
+    appId: '1:98785938897:web:d3fe175943299d35bb64f0',
+    measurementId: 'G-44WX2G9S3L'
+  }
 };
